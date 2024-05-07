6675a8dcd34b2cb88015b2cb55d029b70ffeec8b
Sandra Veronica <sandrasarang77@gmail.com>
2024-05-06 20:50:14 -0500

    Update Logo with new SVG code for logo This commit updates the Logo component with new SVG code. Update fonts and colors. add noise animaton.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,17 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon(), tailwind({ applyBaseStyles: true })],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: ` 
+          @use 'astro-scroll-observer/use-scroll-observer.scss' as *;
+    `,
+        },
+      },
+    },
+  },
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
